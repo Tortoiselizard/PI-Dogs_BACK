710dcbd33ee91f517c841cd692b8774390d5c420
Troy Pernía <troy00pernia@gmail.com>
2023-04-12 10:30:10 -0400

    change routes/dogs: change route /:raza_perro for work with API and DB

--- src/routes/dogs.js
+++ src/routes/dogs.js
@@ -12,8 +12,6 @@ const regNumber = /[^0-9-.– ]/
 
 router.post("/", async(req, res) => {
     const {name, height, weight, life_span, image, temperaments} = req.body
-    // console.log(name, height, weight)
-    // console.log(typeof(name), typeof(height), typeof(weight))
     try {
         if (name && height && weight) {
             const dog = await Dog.findAll({where: {name}})
@@ -32,7 +30,6 @@ router.post("/", async(req, res) => {
             throw new Error("Los atributos: name, height y weight no pueden ser nulos")
         }
     } catch(error) {
-        console.log("entre en error servidor")
         res.status(400).json(error.message)
     }
 })
@@ -61,7 +58,6 @@ router.get("/", async (req, res) => {
                 image: dog.image.url
             }))).catch(error => {throw new Error("Ha ocurrido un problema en el enlace con el servidor de la API")})
         }
-        // console.log()
         if (location === "DB" || location === undefined || location === "TD") {
                 dogsArrayDB = (await Dog.findAll({
                 attributes: {exclude: ["createdAt", "updatedAt"]},
@@ -73,8 +69,7 @@ router.get("/", async (req, res) => {
         }
        
         const dogsArray = [...dogsArrayApi, ...dogsArrayDB]
-        // if (!dogsArray.length) {
-        //     console.log("Esto es antes de enviar el nuevo error")    
+        // if (!dogsArray.length) {    
         //     throw new Error("este es el nuevo error")
         // }
         if (name) {
@@ -89,7 +84,6 @@ router.get("/", async (req, res) => {
         res.status(200).send(arrayDogs)
     }
     catch(error) {
-        // console.log("entre en el catch del servidor con", error)
         res.status(400).json(error.message)
     }
 })
@@ -97,6 +91,9 @@ router.get("/", async (req, res) => {
 router.get("/:raza_perro", async(req, res) => {
     let {raza_perro} = req.params
     raza_perro = raza_perro.trim()
+    // raza_perro = raza_perro.includes(" ")?
+    //     raza_perro.split(" ").map(nombre => nombre[0].toUpperCase() + nombre.slice(1).toLowerCase()).join(" "):
+    //     raza_perro[0].toUpperCase() + raza_perro.slice(1).toLowerCase()
     const RUTA = `https://api.thedogapi.com/v1/breeds/search?q=${raza_perro}&api_key=${YOUR_API_KEY}`
     try {    
         dogFindedAPI = await request({
@@ -104,37 +101,42 @@ router.get("/:raza_perro", async(req, res) => {
             json: true
         })
         .then(data => {
-            if (data.length) {return {
-                id: data[0].id,
-                name: data[0].name,
-                height: data[0].height.metric,
-                weight: data[0].weight.metric,
-                life_span: data[0].life_span,
-                temperament: data[0].temperament,
-                // origin: data[0].origin,
-                // breed_group: data[0].breed_group,
-                image: `https://cdn2.thedogapi.com/images/${data[0].reference_image_id}.jpg`
-                }
+            if (data.length) {return data.map(dog => ({
+                id: dog.id,
+                name: dog.name,
+                height: dog.height.imperial,
+                weight: dog.weight.imperial,
+                life_span: dog.life_span,
+                temperament: dog.temperament,
+                // origin: dog.origin,
+                // breed_group: dog.breed_group,
+                image: dog.reference_image_id && `https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`
+                }))
             } else {
                 return null
             }
         })
         .catch(() => {throw new Error("Ha ocurrido un problema en el enlace con el servidor de la API")})
-        if (dogFindedAPI) {return res.status(200).send(dogFindedAPI)}
-        var dogFindedDB = (await Dog.findOne({
-            where: {name: raza_perro.includes(" ")?
-                raza_perro.split(" ").map(nombre => nombre[0].toUpperCase() + nombre.slice(1).toLowerCase()).join(" "):
-                raza_perro[0].toUpperCase() + raza_perro.slice(1).toLowerCase()},
+        // if (dogFindedAPI) {return res.status(200).send(dogFindedAPI)}
+        var dogFindedDB = (await Dog.findAll({
+            where: {
+                name: {
+                    [Op.like]:  `%${raza_perro}%`
+                }
+            },
             attributes: {exclude: ["createdAt", "updatedAt"]},
             include: Temperament
         }).catch(() => {throw new Error("Ha ocurrido un problema en el enlace con la base de datos de la aplicación")}))
-        console.log(dogFindedDB)
-        if (!dogFindedDB) {return res.status(202).send(`No se ha encontrado la raza de perro "${raza_perro}"`)}
+        // var dogFindedDB = (await Dog.findAll().catch(() => {throw new Error("Ha ocurrido un problema en el enlace con la base de datos de la aplicación")}))
+        const totalDogsFinded = [...dogFindedAPI, ...dogFindedDB].filter(dog => {
+            return typeof(dog.image)==="string"})
+        return res.status(200).send(totalDogsFinded)
+        // if (!dogFindedDB) {return res.status(202).send(`No se ha encontrado la raza de perro "${raza_perro}"`)}
         
-        dogFindedDB = {...dogFindedDB.dataValues, 
-            temperaments: dogFindedDB.temperaments.map(t => t.dataValues.name).join(", ")
-        }
-        return res.status(200).send(dogFindedDB)      
+        // dogFindedDB = {...dogFindedDB.dataValues, 
+        //     temperaments: dogFindedDB.temperaments.map(t => t.dataValues.name).join(", ")
+        // }
+        // return res.status(200).send(dogFindedDB)      
     }
     catch(error) {
         res.status(400).send(error.message)
