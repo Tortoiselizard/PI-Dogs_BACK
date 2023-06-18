4df516a4278b11f10e224a59c7dfd21ec8e4e776
Troy Pernía <troy00pernia@gmail.com>
2023-06-18 15:26:28 -0400

    change routes/temperaments: change format of the file, and put new condition to add new temperaments (line 38, 47). create new const "arrayNewTemperaments" (line 26)

--- src/routes/temperaments.js
+++ src/routes/temperaments.js
@@ -1,55 +1,60 @@
 require('dotenv').config()
-const {Router} = require("express")
-const {Temperament} =require("../db")
-const request = require("request-promise")
-const {YOUR_API_KEY} = process.env
+const { Router } = require('express')
+const { Temperament } = require('../db')
+const request = require('request-promise')
+const { YOUR_API_KEY } = process.env
 
 const router = Router()
 
-router.get("/", async (req, res) => {
-
-    try {
-        const temperamentsArrayDB = await Temperament.findAll()
-        .catch(error => {throw new Error("Ha ocurrido un error en el enlace con la base de datos")})
-        res.status(200).send(temperamentsArrayDB)      
-    } catch(error) {
-        res.status(400).json(error.message)
-    }
-})
-
-router.post("/", async (req, res) => {
-    const RUTA = `https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`
-    const {add} = req.query
-    
+router.get('/', async (req, res) => {
+  try {
     const temperamentsArrayDB = await Temperament.findAll()
-    const arrayTemperaments = temperamentsArrayDB.map(temperament => temperament.name)
-    let newTemperaments = []
-    
-    try {
-        if (add === "update") {
-            const dogsArrayAPI = await request({
-                uri: RUTA,
-                json: true
-            }).then(data => data)
-            .catch(error => {throw new Error("Ha ocurrido un problema con el enlace al servidor de la API")})
-        
-            dogsArrayAPI.forEach(async (dog) => {
-                dog.temperament && dog.temperament.split(", ").forEach(temperament => {
-                    !arrayTemperaments.includes(temperament)? newTemperaments.push({name: temperament}):null
-                })
-            })
-        }
-        if (add === "add") {
-            req.body.forEach(temperament => {
-                !arrayTemperaments.includes(temperament)? newTemperaments.push({name: temperament}):null
-            })
-        }
-        const newTemperamentsToCreate = await Temperament.bulkCreate(newTemperaments)
-        res.status(200).json(newTemperamentsToCreate)
-    }
-    catch(error) {
-        res.status(400).json(error.message)
-    }
+      .catch(error => { throw new Error(error.message) })
+    res.status(200).send(temperamentsArrayDB)
+  } catch (error) {
+    res.status(400).json(error.message)
+  }
 })
 
-module.exports=router
+router.post('/', async (req, res) => {
+  const RUTA = `https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`
+  const { add } = req.query
+
+  const temperamentsArrayDB = await Temperament.findAll()
+  const arrayTemperaments = temperamentsArrayDB.map(temperament => temperament.name)
+  const newTemperaments = []
+  const arrayNewTemperaments = []
+
+  try {
+    if (add === 'update') {
+      const dogsArrayAPI = await request({
+        uri: RUTA,
+        json: true
+      }).then(data => data)
+        .catch(error => { throw new Error(error.message) })
+
+      dogsArrayAPI.forEach(dog => {
+        dog.temperament && dog.temperament.split(', ').forEach(temperament => {
+          if (!arrayTemperaments.includes(temperament) && !arrayNewTemperaments.includes(temperament)) {
+            newTemperaments.push({ name: temperament })
+            arrayNewTemperaments.push(temperament)
+          }
+        })
+      })
+    }
+    if (add === 'add') {
+      req.body.forEach(temperament => {
+        if (!arrayTemperaments.includes(temperament) && !arrayNewTemperaments.includes(temperament)) {
+          newTemperaments.push({ name: temperament })
+          arrayNewTemperaments.push(temperament)
+        }
+      })
+    }
+    const newTemperamentsToCreate = await Temperament.bulkCreate(newTemperaments)
+    res.status(200).json(newTemperamentsToCreate)
+  } catch (error) {
+    res.status(400).json(error.message)
+  }
+})
+
+module.exports = router
